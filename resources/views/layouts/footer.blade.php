a2fbc9ef00a0ca1b2666038a15ad72cd99ce98bd
Shabnata <mer.natalie@gmail.com>
2017-10-09 13:59:47 +0300

    Header/Footer/LeftMenu/Css/JS/Favicon - initial layouts Initialized Auth (Laravel default) AppServiceProvider PagesController

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+    <p style="color: #ffffff;">&copy 2015 Easy Admin Panel. All Rights Reserved | Design by <a style="color: #ffffff;" href="https://w3layouts.com/" target="_blank">w3layouts.</a></p>
+</footer>
