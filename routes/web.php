b8312a5da3c6dae0742acca629b78256ea3bad51
Shabnata <mer.natalie@gmail.com>
2017-10-09 13:59:47 +0300

    Header/Footer/LeftMenu/Css/JS/Favicon - initial layouts Initialized Auth (Laravel default) AppServiceProvider PagesController

--- routes/web.php
+++ routes/web.php
@@ -15,21 +15,6 @@
 //    return view('layouts/master');
 //});
 Route::get('/' , 'PagesController@index');
-Route::get('/addCat' , ['uses'=>'CatController@addCat']);
-Route::get('/catPage' , ['uses'=>'CatController@catPage']);
-//Route::get('/addCat' ,['uses'=>'PagesController@addCat']);
-//Route::get('/getCatBreedInfo/{breed_name}' , ['uses'=>'CatController@breedInfo']);
-Route::get('/getCatBreedInfo' , ['uses'=>'CatController@breedInfo']);
-Route::get('/autocompleteBreed', 'CatController@autocomplete');
-
-Route::get('/catVetPage', 'CatController@catVetPage');
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-
-
-//added by michael
-Route::post('addcat',['uses' => 'CatController@store']);
-Route::get('/temp' , 'CatController@myCats');
