7a0d0280ad5c9b8d0c67fccd6a707da6d7e7175d
Shabnata <mer.natalie@gmail.com>
2017-10-12 16:10:16 +0300

    add cat page in process

--- resources/views/pages/addCat.blade.php
+++ resources/views/pages/addCat.blade.php
@@ -1,209 +1,90 @@
 @extends('layouts.master')
 @section('content')
-    <script>
-        $(document).ready(function () {
-            $('#title').html('Add a new cat to the family:');
-        });
-    </script>
     <div id="page-wrapper">
         <div class="graphs">
-            <h3 class="blank1">Your cat familly:</h3>
-            <div class="col-sm-12">
-                <div class="row">
-                    <div class="col-md-4"  style="padding-bottom: 25px;">
-                        <div class="r3_counter_box">
-                            <i class="fa" style="width: 150px; margin-left: -30px"><img
-                                    src="https://cdn2.iconfinder.com/data/icons/cat-power/128/cat_drunk.png"
-                                    width="100px"></i>
-                            <div  align="center" class="stats" style="padding-bottom: 10px">
-                                <div class="grow groww">
-                                    <p>Cat name</p>
-                                </div>
+            <h3 class="blank1">Add a new cat:</h3>
+            <div class="tab-content">
+                <div class="tab-pane active" id="horizontal-form">
+                    <form class="form-horizontal">
+                        <div class="form-group">
+                            <label for="focusedinput" class="col-sm-2 control-label">Name:</label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control1" id="focusedinput" placeholder="">
+                            </div>
+                            {{--<div class="col-sm-2 jlkdfj1">--}}
+                            {{--<p class="help-block">Your help text!</p>--}}
+                            {{--</div>--}}
+                        </div>
+                        <div class="form-group">
+                            <label for="focusedinput" class="col-sm-2 control-label">Breed:</label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control1" id="breed" placeholder="" onKeyup="trackChange(this.value)">
+                                Info:
+                                <div id="wikiInfo"></div>
+                                Url:
+                                <a id="wikiLink" href=""></a>
 
-                                <form class="form-horizontal">
-                                    <table class="catCardTable">
-                                        <tbody>
-                                        <tr>
-                                            <td>Name:</td>
-                                            <td>Ellie</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Breed:</td>
-                                            <td>Devon rex</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Wiki page:</td>
-                                            <td>some link</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Gender:</td>
-                                            <td>Female</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Birthday:</td>
-                                            <td>2007-10-10</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Current weight:</td>
-                                            <td>5</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Target weight:</td>
-                                            <td>4</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Daily calories:</td>
-                                            <td>400</td>
-                                        </tr>
-                                        </tbody>
-                                    </table>
-                                        <div class="row" style="float: right;">
-                                                <ul class="nav nav-pills">
-                                                    {{--Edit will take to catPage--}}
-                                                    <li class="menu-list"><a href="#"><i class="lnr lnr-pencil editValues" onclick=""></i></a></li>
-                                                </ul>
-
-                                        </div>
-                                    <!-- /.table-responsive -->
-                                </form>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-md-4"  style="padding-bottom: 25px;">
-                        <div class="r3_counter_box">
-                            <i class="fa" style="width: 150px; margin-left: -30px"><img
-                                    src="https://cdn3.iconfinder.com/data/icons/cat-force/128/cat_paper.png"
-                                    width="100px"></i>
-                            <div align="center" class="stats" style="padding-bottom: 10px">
-                                <div class="grow grow1 groww">
-                                    <p>Cat name</p>
-                                </div>
-                                <form class="form-horizontal">
-                                    <table class="catCardTable">
-                                        <tbody>
-                                        <tr>
-                                            <td>Name:</td>
-                                            <td>Ellie</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Breed:</td>
-                                            <td>Devon rex</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Wiki page:</td>
-                                            <td>some link</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Gender:</td>
-                                            <td>Female</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Birthday:</td>
-                                            <td>2007-10-10</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Current weight:</td>
-                                            <td>5</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Target weight:</td>
-                                            <td>4</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Daily calories:</td>
-                                            <td>400</td>
-                                        </tr>
-                                        </tbody>
-                                    </table>
-                                    <div class="row" style="float: right;">
-                                        <ul class="nav nav-pills">
-                                            {{--Edit will take to catPage--}}
-                                            <li class="menu-list"><a href="#"><i class="lnr lnr-pencil editValues" onclick=""></i></a></li>
-                                        </ul>
 
-                                    </div>
-                                    <!-- /.table-responsive -->
-                                </form>
+                        <div class="form-group">
+                            @include('layouts.wikiInfo')
+                        </div>
+                        <div class="form-group">
+                            <label for="radio" class="col-sm-2 control-label">Gender:</label>
+                            <div class="col-sm-8">
+                                <div class="radio-inline"><label><input type="radio" name="gender" value="male" checked>Male<br></label>
+                                </div>
+                                <div class="radio-inline"><label><input type="radio" name="gender" value="female">Female<br></label>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-md-4"  style="padding-bottom: 25px;">
-                        <div class="r3_counter_box">
-                            <i class="fa" style="width: 150px; margin-left: -30px"><img
-                                    src="https://cdn3.iconfinder.com/data/icons/cat-force/128/cat_upsidedown.png"
-                                    width="100px"></i>
-                            <div align="center" class="stats" style="padding-bottom: 10px">
-                                <div class="grow grow3 groww">
-                                    <p>Cat name</p>
-                                </div>
-                                <form class="form-horizontal">
-                                    <table class="catCardTable">
-                                        <tbody>
-                                        <tr>
-                                            <td>Name:</td>
-                                            <td>Ellie</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Breed:</td>
-                                            <td>Devon rex</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Wiki page:</td>
-                                            <td>some link</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Gender:</td>
-                                            <td>Female</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Birthday:</td>
-                                            <td>2007-10-10</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Current weight:</td>
-                                            <td>5</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Target weight:</td>
-                                            <td>4</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Daily calories:</td>
-                                            <td>400</td>
-                                        </tr>
-                                        </tbody>
-                                    </table>
-                                    <div class="row" style="float: right;">
-                                        <ul class="nav nav-pills">
-                                            {{--Edit will take to catPage--}}
-                                            <li class="menu-list"><a href="#"><i class="lnr lnr-pencil editValues" onclick=""></i></a></li>
-                                        </ul>
+                        <div class="form-group">
+                            @include('layouts.datePicker')
+                        </div>
 
-                                    </div>
-                                    <!-- /.table-responsive -->
-                                </form>
+                        <div class="form-group">
+                            <label for="smallinput" class="col-sm-2 control-label label-input-sm">Current weight:</label>
+                            <div class="col-sm-8">
+                                <input type="number"  step="any" min="0" max="100" class="form-control1 input-sm" id="currentWeight" placeholder="">
                             </div>
                         </div>
-                    </div>
+
+                        <div class="form-group">
+                            <label for="smallinput" class="col-sm-2 control-label label-input-sm">Target weight:</label>
+                            <div class="col-sm-8">
+                                <input type="number"  step="any" min="0" max="100" class="form-control1 input-sm" id="targetWeight" placeholder="">
+                            </div>
+                        </div>
 
 
-
-
-
-
+                        <div class="form-group">
+                            <label for="smallinput" class="col-sm-2 control-label label-input-sm">Daily callories:</label>
+                            <div class="col-sm-8">
+                                <input type="number"  step="any" min="0" max="1000" class="form-control1 input-sm" id="dailyCallories" placeholder="">
+                            </div>
+                        </div>
+                    </form>
                 </div>
+                <form class="form-horizontal">
+                    <div class="form-group">
+                        <label for="exampleInputFile">File input</label>
+                        <input type="file" id="exampleInputFile">
+                        <p class="help-block">Example block-level help text here.</p>
+                    </div>
+                    <div class="panel-footer">
+                        <div class="row">
+                            <div class="col-sm-8 col-sm-offset-2">
+                                <button class="btn-success btn">Submit</button>
+                                <button class="btn-default btn">Cancel</button>
+                                <button class="btn-inverse btn">Reset</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </div>
-            <div class="row" style="margin: 25px">
-                {{--spacing div please don't remove //Natalie--}}
-            </div>
-            <hr>
-            @include("layouts.catFields")
 
-            <div class="row" style="margin: 25px">
-                {{--spacing div please don't remove //Natalie--}}
-            </div>
 
         </div>
     </div>
-
 @endsection
