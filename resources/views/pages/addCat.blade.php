5e95aa4aeeee17884a471041731e97933d997128
Shabnata <mer.natalie@gmail.com>
2017-10-13 01:40:20 +0300

    Cat page view

--- resources/views/pages/addCat.blade.php
+++ resources/views/pages/addCat.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.master')
 @section('content')
+
     <div id="page-wrapper">
         <div class="graphs">
             <h3 class="blank1">Add a new cat:</h3>
@@ -11,19 +12,35 @@
                             <div class="col-sm-8">
                                 <input type="text" class="form-control1" id="focusedinput" placeholder="">
                             </div>
-                            {{--<div class="col-sm-2 jlkdfj1">--}}
-                            {{--<p class="help-block">Your help text!</p>--}}
-                            {{--</div>--}}
+                        </div>
+
+
+                        <div class="form-group">
+                            <label for="breed_selection" class="col-sm-2 control-label"
+                                   id="breed_selection">Breed</label>
+                            <div class="col-sm-8">
+                                <select name="breed" id="breed" class="form-control1"
+                                        onchange="trackChange(this.value)">
+                                    <option value="" name=""></option>
+                                    @foreach ($breeds as $breed)
+                                        <option value="{!! $breed['breed_name'] !!}"
+                                                name="{!! $breed['link'] !!}">{!! $breed['breed_name'] !!}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="focusedinput" class="col-sm-2 control-label">Wiki page:</label>
+                            <div class="col-sm-8" style="margin-top: 6px">
+                                <a id="wikiLink" href=""></a>
+                            </div>
+
                         </div>
                         <div class="form-group">
-                            <label for="focusedinput" class="col-sm-2 control-label">Breed:</label>
-                            <div class="col-sm-8">
-                                <input type="text" class="form-control1" id="breed" placeholder="" onKeyup="trackChange(this.value)">
-                                Info:
+                            <label for="focusedinput" class="col-sm-2 control-label">Wiki info:</label>
+                            <div class="col-sm-8" style="margin-top: 6px">
                                 <div id="wikiInfo"></div>
-                                Url:
-                                <a id="wikiLink" href=""></a>
-
                             </div>
                         </div>
 
@@ -44,47 +61,60 @@
                         </div>
 
                         <div class="form-group">
-                            <label for="smallinput" class="col-sm-2 control-label label-input-sm">Current weight:</label>
+                            <label for="smallinput" class="col-sm-2 control-label label-input-sm">Current
+                                weight:</label>
                             <div class="col-sm-8">
-                                <input type="number"  step="any" min="0" max="100" class="form-control1 input-sm" id="currentWeight" placeholder="">
+                                <input type="number" step="any" min="0" max="100" class="form-control1 input-sm"
+                                       id="currentWeight" placeholder="">
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label for="smallinput" class="col-sm-2 control-label label-input-sm">Target weight:</label>
                             <div class="col-sm-8">
-                                <input type="number"  step="any" min="0" max="100" class="form-control1 input-sm" id="targetWeight" placeholder="">
+                                <input type="number" step="any" min="0" max="100" class="form-control1 input-sm"
+                                       id="targetWeight" placeholder="">
                             </div>
                         </div>
 
 
                         <div class="form-group">
-                            <label for="smallinput" class="col-sm-2 control-label label-input-sm">Daily callories:</label>
+                            <label for="smallinput" class="col-sm-2 control-label label-input-sm">Daily
+                                callories:</label>
                             <div class="col-sm-8">
-                                <input type="number"  step="any" min="0" max="1000" class="form-control1 input-sm" id="dailyCallories" placeholder="">
+                                <input type="number" step="any" min="0" max="1000" class="form-control1 input-sm"
+                                       id="dailyCallories" placeholder="">
+                            </div>
+                        </div>
+                    </form>
+                    <form class="form-horizontal">
+
+
+
+                        <div class="form-group">
+                            <label for="profilePicture" class="col-sm-2 control-label">Profile picture:</label>
+                            <div class="col-sm-8">
+                                <input type="file" id="profilePicture"  class="filestyle" data-buttonBefore="true" style="margin-top: 6px">
+                            {{--<p class="help-block">Example block-level help text here.</p>--}}
+                            </div>
+
+                        </div>
+                        <div class="">
+                            <div class="row">
+                                <div class="col-sm-8 col-sm-offset-2">
+                                    <button class="btn-success btn">Submit</button>
+                                    <button class="btn-default btn">Cancel</button>
+                                    <button class="btn-inverse btn">Reset</button>
+                                </div>
                             </div>
                         </div>
                     </form>
                 </div>
-                <form class="form-horizontal">
-                    <div class="form-group">
-                        <label for="exampleInputFile">File input</label>
-                        <input type="file" id="exampleInputFile">
-                        <p class="help-block">Example block-level help text here.</p>
-                    </div>
-                    <div class="panel-footer">
-                        <div class="row">
-                            <div class="col-sm-8 col-sm-offset-2">
-                                <button class="btn-success btn">Submit</button>
-                                <button class="btn-default btn">Cancel</button>
-                                <button class="btn-inverse btn">Reset</button>
-                            </div>
-                        </div>
-                    </div>
-                </form>
+
             </div>
 
 
         </div>
     </div>
+
 @endsection
