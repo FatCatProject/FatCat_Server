70479a419f91a27f535c4f43029fd256f7e7104b
Shabnata <mer.natalie@gmail.com>
2017-10-12 16:10:16 +0300

    add cat page in process

--- routes/web.php
+++ routes/web.php
@@ -15,40 +15,7 @@
 //    return view('layouts/master');
 //});
 Route::get('/' , 'PagesController@index');
-Route::get('/addCat' , ['uses'=>'CatController@addCat']);
-
-
-
-//Route::get('/addCat' ,['uses'=>'PagesController@addCat']);
-//Route::get('/getCatBreedInfo/{breed_name}' , ['uses'=>'CatController@breedInfo']);
-Route::get('/getCatBreedInfo' , ['uses'=>'CatController@breedInfo']);
-Route::get('/autocompleteBreed', 'CatController@autocomplete');
-
-
-
+Route::get('/addCat' , 'PagesController@addCat');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-//added by michael
-//works and needed
-Route::post('/addcat',['uses' => 'CatController@store']);
-Route::put('/catPage/editcat',['uses' => 'CatController@update']);
-Route::get('/catPage/{id}/{date?}',['uses' =>'CatController@catPage']);
-Route::get('/catVetPage/{id}/{year?}', 'VetController@catVetPage');
-Route::post('/addvetlog',['uses' => 'VetController@store']);
-Route::get('/shoppingPage/{year?}',['uses' =>'ShopController@shoppingPage']);
-Route::post('/addShopping',['uses' => 'ShopController@storeShopLog']);
-
-
-
-//tests
-Route::get('/temp/{year}' , 'VetController@yearlyVetLogs');
-
-
-Route::get('/homePage', 'PagesController@home');
-Route::get('/shoppingPage', 'PagesController@shop');
-Route::get('/shopsPage', 'PagesController@shopList');
-Route::get('/userPage', 'PagesController@userPage');
-Route::get('/boxManagePage', 'PagesController@boxManagePage');
-Route::get('/editBoxPage', 'PagesController@editBoxPage');
