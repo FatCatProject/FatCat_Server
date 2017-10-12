4be3b3a40d01f69855a38a576ca996e24a7f3a8c
Shabnata <mer.natalie@gmail.com>
2017-10-12 16:10:16 +0300

    add cat page in process

--- resources/views/layouts/styling.blade.php
+++ resources/views/layouts/styling.blade.php
@@ -1,25 +1,3 @@
-
-<link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-<script src="js/jquery-1.10.2.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.js"></script>
-{{--<script src="js/bootstrap.js"></script>--}}
-<script src="js/bootstrap.min.js"></script>
-
-<script src="js/Chart.js"></script>
-<script src="js/classie.js"></script>
-<script src="js/jquery.flexisel.js"></script>
-<script src="js/jquery.flot.min.js"></script>
-<script src="js/skycons.js"></script>
-<script src="js/uisearch.js"></script>
-<script src="js/wow.min.js"></script>
-{{--<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>--}}
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); }</script>
-<script>
-    new WOW().init();
-</script>
-
-
 <link href="css/animate.css" rel='stylesheet' type='text/css' />
 <link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
 <link href="css/font-awesome.css" rel='stylesheet' type='text/css' />
@@ -27,13 +5,28 @@
 <link href="css/icon-font.min.css" rel='stylesheet' type='text/css' />
 <link href="css/style.css" rel='stylesheet' type='text/css' />
 <link href="css/bootstrap-datetimepicker.css" rel='stylesheet' type='text/css' />
-<link href='//fonts.googleapis.com/css?family=Cabin:400,400italic,500,500italic,600,600italic,700,700italic' rel='stylesheet' type='text/css' />
-<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/themes/base/minified/jquery-ui.min.css" type="text/css" />
+<link href='//fonts.googleapis.com/css?family=Cabin:400,400italic,500,500italic,600,600italic,700,700italic' rel='stylesheet' type='text/css'>
 
-{{--<script src="js/jquery.nicescroll.js"></script>--}}
-{{--<script src="js/scripts.js"></script>--}}
+
+<script src="js/bootstrap.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/Chart.js"></script>
+<script src="js/classie.js"></script>
+<script src="js/jquery.flexisel.js"></script>
+<script src="js/jquery.flot.min.js"></script>
+<script src="js/jquery.nicescroll.js"></script>
+<script src="js/jquery-1.10.2.min.js"></script>
+<script src="js/scripts.js"></script>
+<script src="js/skycons.js"></script>
+<script src="js/uisearch.js"></script>
+<script src="js/wow.min.js"></script>
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 
 
 
 {{--index--}}
-
+<script src="js/bootstrap.min.js"></script>
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<script>
+    new WOW().init();
+</script>
