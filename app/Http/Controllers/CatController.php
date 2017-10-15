25f839359c70025e4424389666d8924ad2897deb
Shabnata <mer.natalie@gmail.com>
2017-10-15 18:02:03 +0300

    cat breeds autocomplete

--- app/Http/Controllers/CatController.php
+++ app/Http/Controllers/CatController.php
@@ -5,21 +5,54 @@ namespace App\Http\Controllers;
 use App\CatBreed;
 use App\Cat;
 use App\FeedingLog;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class CatController extends Controller
 {
-    public function breeds()
+    public function addCat()
     {
-        $breeds = CatBreed::all();
+        $breeds = DB::table('cat_breeds')->pluck('breed_name');
         return view('pages.addCat', compact('breeds'));
     }
 
-    public function catPage(){
+    public function breedInfo(Request $request)
+    {
+        if ($request->has('breed_name')) {
+            $breed_name = $request->input('breed_name');
+
+            $breed = DB::table('cat_breeds')->where('breed_name', $breed_name)->first();
+            if (is_null($breed)) {
+                $breed = DB::table('cat_breeds')->where('breed_name', 'Other')->first();
+            }
+
+        } else {
+            $breed = DB::table('cat_breeds')->where('breed_name', 'Other')->first();
+        }
+
+        return response()->json([
+            'breed_name' => $breed->breed_name,
+            'link' => $breed->link,
+            'description' => $breed->description
+        ]);
+    }
+
+    public function catPage()
+    {
 //  IMPORTANT:  getting all cats and all logs - just for view checking purposes !!!
         $catInfo = Cat::all();
         $catLogs = FeedingLog::all();
-        return view('pages.catPage',compact('catInfo'),compact('catLogs'));
+        return view('pages.catPage', compact('catInfo'), compact('catLogs'));
 
     }
+    public function autocomplete(Request $request){
+        $breedSearch = $request->input('searchTerm');
+        $queries = DB::table('cat_breeds')
+            ->where('breed_name', 'LIKE', '%'.$breedSearch.'%')->pluck('breed_name');
+
+        return response()->json($queries);
+    }
+
+
 }
