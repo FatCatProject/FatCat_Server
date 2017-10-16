e328f3ccb447e91b728470e5f9288e998d842c04
Shabnata <mer.natalie@gmail.com>
2017-10-16 13:28:31 +0300

    css/js fixed

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -36,7 +36,7 @@
     <!-- main content end-->
 </section>
 
-{{--<script src="js/jquery.nicescroll.js"></script>--}}
-{{--<script src="js/scripts.js"></script>--}}
+<script src="js/jquery.nicescroll.js"></script>
+<script src="js/scripts.js"></script>
 </body>
 </html>
