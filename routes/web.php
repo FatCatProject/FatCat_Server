c7526e402ff2fc2ef7dc6c23d9f3969579e30b00
Michael <michaeldubers@gmail.com>
2017-10-18 17:13:19 +0300

    commit before working with auth, incase ill ruin everything

--- routes/web.php
+++ routes/web.php
@@ -50,3 +50,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 //added by michael
 Route::post('addcat',['uses' => 'CatController@store']);
+Route::get('/temp' , 'CatController@myCats');
