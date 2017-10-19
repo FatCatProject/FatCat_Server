5d78ebf79a4270a228c7c2b5ffc72d78a24747fe
Michael <michaeldubers@gmail.com>
2017-10-19 15:36:16 +0300

    commit before rebase

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,25 @@ Route::get('/catPage' , ['uses'=>'CatController@catPage']);
 Route::get('/getCatBreedInfo' , ['uses'=>'CatController@breedInfo']);
 Route::get('/autocompleteBreed', 'CatController@autocomplete');
 
-Route::get('/catVetPage', 'CatController@catVetPage');
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+
+//added by michael
+Route::post('addcat',['uses' => 'CatController@store']);
+Route::get('/temp' , 'CatController@myCats');
+Route::get('catPage/{id}',['uses' => 'CatController@dailyEating']);
+
+//temp
+Route::get('/temp2',['uses' => 'CatController@create1']);
+Route::post('foodboxes',['uses' => 'CatController@store1']);
+Route::get('/temp3',['uses' => 'CatController@create2']);
+Route::post('cards',['uses' => 'CatController@store2']);
+
 
 Auth::routes();
 
