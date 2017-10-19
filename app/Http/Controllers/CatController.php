59f4983e0f2188dc63b917e9c96ab9ee911f8d46
Michael <michaeldubers@gmail.com>
2017-10-19 15:36:16 +0300

    commit before rebase

--- app/Http/Controllers/CatController.php
+++ app/Http/Controllers/CatController.php
@@ -6,6 +6,8 @@ use DateTime;
 use App\User;
 use App\CatBreed;
 use App\Cat;
+use App\Card;
+use App\Foodbox;
 use App\Http\Controllers\Auth;
 use App\FeedingLog;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
@@ -46,7 +48,8 @@ class CatController extends Controller
 //  IMPORTANT:  getting all cats and all logs - just for view checking purposes !!!
         $catInfo = Cat::all();
         $catLogs = FeedingLog::all();
-        return view('pages.catPage', compact('catInfo'), compact('catLogs'));
+        $mycats = $this->myCats();
+        return view('pages.catPage', compact('catInfo'), compact('catLogs'),compact($mycats));
 
     }
     public function autocomplete(Request $request){
@@ -71,16 +74,57 @@ class CatController extends Controller
 
         $id = DB::table('cats')->insertGetId(
             ['user_email'=>$currentUser->email ,'cat_name'=>$request->cat_name ,'profile_picture'=>$profile_picture,
-                'dob'=>$dob ,'gender'=>$request->gender ,'cat_breed'=>$request->cat_breed , 'current_weight'=>$request->current_weight,
-                'target_weight'=>$request->target_weight , 'daily_calories'=>$request->daily_calories , 'created_at'=>$now ,
-                'updated_at'=>$now]
+                'dob'=>$dob ,'gender'=>$request->gender ,'cat_breed'=>$request->cat_breed ,
+                'current_weight'=>$request->current_weight, 'target_weight'=>$request->target_weight ,
+                'daily_calories'=>$request->daily_calories , 'created_at'=>$now , 'updated_at'=>$now]
         );
         }
         return redirect()->back()->with('status',$status);
     }
 
-    public function myCats(){
-        $currentUserCats = auth()->user()->cats();
-        dd($currentUserCats);
+    //Returns all the cat names of logged in user
+    public static function myCats(){
+        return(auth()->user()->cats);
     }
+
+    public function dailyEating($id,$date){
+        $cat = Cat::find($id);
+        $card_ids = array();
+        $card_ids = DB::table('cards')->where('cat_id',$cat->id)->get();
+        $dailyFeedingLogs= Array();
+        foreach ($card_ids as $card_id){
+
+            $FeedingLogs = DB::table('feeding_logs')->where(
+                ['card_id',$card_id->card_id]
+                ['open_time',]
+            )->get();
+
+            array_push($dailyFeedingLogs,$FeedingLogs);
+
+
+        }
+        return $dailyFeedingLogs;
+
+    }
+
+
+    //everything bellow is temp
+    public function create1(){
+        return view('temp2');
+    }
+    public function store1(Request $request){
+        Foodbox::create($request->all());
+        return redirect()->back();
+    }
+    public function create2(){
+        return view('temp3');
+    }
+    public function store2(Request $request){
+        Card::create($request->all());
+        return redirect()->back();
+    }
+
+
+
+
 }
