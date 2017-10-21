51bbb77d4bff6e1f6ff596425677c445cae8ad31
Michael <michaeldubers@gmail.com>
2017-10-21 21:14:19 +0300

    route cleanup
    feeding logs work with SQL instead of code, sorted by open time

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,6 @@ Route::get('/' , 'PagesController@index');
 Route::get('/addCat' , ['uses'=>'CatController@addCat']);
 
 
-Route::get('/catPage/{id}',['uses' =>'CatController@catPage']);  // By Natalie
-
-
 
 //Route::get('/addCat' ,['uses'=>'PagesController@addCat']);
 //Route::get('/getCatBreedInfo/{breed_name}' , ['uses'=>'CatController@breedInfo']);
@@ -36,13 +33,14 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 
 //added by michael
+//works and needed
 Route::post('/addcat',['uses' => 'CatController@store']);
 Route::put('/catPage/editcat',['uses' => 'CatController@update']);
-Route::get('/temp' , 'CatController@test');
-Route::get('temp/{string}',['uses' => 'CatController@stringToDateTime']);
-Route::get('catPage/{id}/{date}',['uses' => 'CatController@dailyFeedingLogs']);
+Route::get('/catPage/{id}',['uses' =>'CatController@catPage']);
+//tests
+Route::get('/temp/{id}' , 'CatController@allReportsByID');
+Route::get('/temp/{id}/{date}' , 'CatController@dailyFeedingLogs');
+//not in use
+//Route::get('temp/{string}',['uses' => 'CatController@stringToDateTime']);
+//Route::get('catPage/{id}/{date}',['uses' => 'CatController@dailyFeedingLogs']);
 
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
