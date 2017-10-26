615767a9a594d604b624ddbcc553a3365bfc94b9
Michael <michaeldubers@gmail.com>
2017-10-26 11:10:41 +0300

    The problem with transferring logs to catVetPage Fixed Sql query updated.

--- app/Http/Controllers/VetController.php
+++ app/Http/Controllers/VetController.php
@@ -6,10 +6,6 @@ use Illuminate\Http\Request;
 use App\Cat;
 use DateTime;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Http\UploadedFile;
-use Illuminate\Support\Facades\Input;
-use function PHPSTORM_META\type;
-use Image;
 
 class VetController extends Controller
 {
@@ -18,54 +14,43 @@ class VetController extends Controller
             $year = new DateTime();
             $year = explode('-',$year->format('Y-m-d'))[0];
         }
-
-        $cat = Cat::find($id);
-        $currentUser = auth()->user();
-
-        $vetLogs = $this->yearlyVetLogs($year,$cat->cat_name,$currentUser->email);
-        $totalExpenses = 0;
         $expensesPerMonth = array("1"=>0,"2"=>0,"3"=>0,"4"=>0,"5"=>0,"6"=>0,"7"=>0,"8"=>0,
             "9"=>0, "10"=>0,"11"=>0,"12"=>0);
+        $cat = Cat::find($id);
+        $currentUser = auth()->user();
+        $vetLogs = $this->yearlyVetLogs($year,$cat->cat_name,$currentUser->email);
+        dd($vetLogs);
+        $totalExpenses = 0;
         foreach ($vetLogs as $vetLog){
             $logDate = explode(" ",$vetLog->visit_date);
             $logMonth = intval((explode("-",$logDate[0]))[1]);
             $expensesPerMonth[$logMonth]= $expensesPerMonth[$logMonth]+$vetLog->price;
-            $totalExpenses = $totalExpenses + $vetLog->price;
-        }
 
-        return view('pages.catVetPage',compact('cat'),compact('expensesPerMonth'))
-            ->with('vetLogs',$vetLogs)->with('totalExpenses',$totalExpenses);
+        }
+        //dd($allVetLogs);
+        return view('pages.catVetPage',compact('cat'),compact('expensesPerMonth'))->with('vetLogs',$vetLogs);
     }
 
-    //fixme
-    //base_64 encoded encodes to xammp folder.
     public function store(Request $request){
-        $cat = Cat::find($request->id);
-        if($request->hasFile('prescription_picture')){
-            $file= $request->file('prescription_picture');
-            $fileName = $cat->id . ".png";
-            Image::make($file)->save(public_path('/uploads/prescription_picturses/' . $fileName));
-
-        }else
-            $encodedPrescriptionPicture = "";
-
-        //adds the same cat vet log after refresh
         $status="success";
+        $cat = Cat::find($request->id);
         date_default_timezone_set('Asia/Jerusalem');
         $currentUser = auth()->user();
-        if($currentUser == null || $request->cat_name == null || $request->visit_date == null){
-            $status = "Failed, something missing";
+        if($currentUser == null){
+            $status = "Failed, you need to sign in";
         }else{
+            $prescription_picture=base64_encode($request->profile_picture);
             $visit_date = (new DateTime($request->visit_date))->format('Y-m-d');
             $now = new DateTime();
             DB::table('cats_vet_logs')->insert(
                     ['user_email'=>$currentUser->email ,'visit_date'=>$visit_date ,'subject'=>$request->subject,
                         'description'=>$request->description ,'clinic_name'=>$request->clinic_name ,
-                        'prescription_picture'=>$fileName, 'price'=>$request->price ,
+                        'prescription_picture'=>$prescription_picture, 'price'=>$request->price ,
                         'cat_name'=>$cat->cat_name]
                 );
+
         }
-        return $this->catVetPage($request->id,$year = 'null');
+        return view('pages.catVetPage',['id'=>$cat->id,$year='null'],compact('cat'));
     }
 
     public function yearlyVetLogs($year,$name,$user_email){
