c77da28d67abce990aea8f4ce5dc0dd659652d69
Shabnata <mer.natalie@gmail.com>
2017-11-05 17:33:36 +0200

    footer links

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,3 @@
 <footer>
-    <p style="color: #ffffff;">&copy 2015 Easy Admin Panel. All Rights Reserved | Design by <a style="color: #ffffff;" href="https://w3layouts.com/" target="_blank">w3layouts.</a></p>
-    <p>Stock images from http://www.iconka.com</p>
+    <p style="font-size: 8px">&copy 2015 Easy Admin Panel. All Rights Reserved | Design by <a style="color: #ffffff;" href="https://w3layouts.com/" target="_blank">w3layouts.</a>Stock images from http://www.iconka.com</p>
 </footer>
