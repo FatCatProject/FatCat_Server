b5055ca58df91fd1bfbed7fa453ba7536ed674ff
SheyDenis <denis.sheyer@gmail.com>
2017-11-05 16:27:48 +0200

    Added default cat profile picture.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,4 @@
 <footer>
     <p style="color: #ffffff;">&copy 2015 Easy Admin Panel. All Rights Reserved | Design by <a style="color: #ffffff;" href="https://w3layouts.com/" target="_blank">w3layouts.</a></p>
+    <p>Stock images from http://www.iconka.com</p>
 </footer>
