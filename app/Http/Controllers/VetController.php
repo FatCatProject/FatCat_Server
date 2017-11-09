5991cad04ba457f9d696396d7e276b2979a4d3f1
Michael <michaeldubers@gmail.com>
2017-11-09 23:33:22 +0200

    close #73,#86

--- app/Http/Controllers/VetController.php
+++ app/Http/Controllers/VetController.php
@@ -27,25 +27,25 @@ class VetController extends Controller
         $vet_logs = $this->yearlyVetLogs($year, $cat->cat_name, $current_user->email);
         $expenses_per_month = $this->expensesPerMonth($vet_logs);
         $month_expenses = "[";
-        foreach ($expenses_per_month as $expense){
-            $month_expenses .= $expense.",";
+        foreach ($expenses_per_month as $expense) {
+            $month_expenses .= $expense . ",";
         }
         $month_expenses .= "]";
         $total_expenses = $this->spentDuringYear($expenses_per_month);
 
         $vet_prescription_pictures = [];
 
-        foreach($vet_logs as $log){
-            if(!empty($log->prescription_picture)){
-                $prescription_picture_path = str_replace(["@", "."], "_", $current_user->email)."/".$log->prescription_picture;
-                if(Storage::disk("user_pictures")->exists($prescription_picture_path)){
-                    $vet_prescription_pictures[$log->id] = "data:image/png;base64,".base64_encode(
+        foreach ($vet_logs as $log) {
+            if (!empty($log->prescription_picture)) {
+                $prescription_picture_path = str_replace(["@", "."], "_", $current_user->email) . "/" . $log->prescription_picture;
+                if (Storage::disk("user_pictures")->exists($prescription_picture_path)) {
+                    $vet_prescription_pictures[$log->id] = "data:image/png;base64," . base64_encode(
                             Storage::disk("user_pictures")->get($prescription_picture_path)
                         );
-                }else{
+                } else {
                     $vet_prescription_pictures[$log->id] = "No prescription picture";
                 }
-            }else{
+            } else {
                 $vet_prescription_pictures[$log->id] = "No prescription picture";
             }
         }
@@ -68,8 +68,8 @@ class VetController extends Controller
         $vet_log = new \App\CatVetLog(
             [
                 "user_email" => $current_user->email,
-                "cat_name" =>  Cat::find($request->id)->cat_name,
-                "visit_date" =>$request->date
+                "cat_name" => Cat::find($request->id)->cat_name,
+                "visit_date" => $request->date
             ]
         );
         $vet_log->subject = $request->subject;
@@ -77,24 +77,24 @@ class VetController extends Controller
         $vet_log->clinic_name = $request->clinic_name;
         $vet_log->prescription_picture = $request->prescription_picture;
         $vet_log->price = $request->price;
-        try{
-            if(!empty($request->prescription_picture)){
+        try {
+            if (!empty($request->prescription_picture)) {
                 $vet_log->prescription_picture = str_replace(
                         ["@", "."],
                         "_",
-                        $current_user->email."_".$vet_log->visit_date
-                    ).".".$request->prescription_picture->getClientOriginalExtension();
+                        $current_user->email . "_". $vet_log->cat_name ."_" . $vet_log->visit_date
+                    ) . "." . $request->prescription_picture->getClientOriginalExtension();
             }
 
             $vet_log->save();
-            if (!empty($vet_log->prescription_picture)){
+            if (!empty($vet_log->prescription_picture)) {
                 Storage::disk("user_pictures")->putFileAs(
                     str_replace(["@", "."], "_", $current_user->email),
                     $request->prescription_picture,
                     $vet_log->prescription_picture
                 );
             }
-        }catch(QueryException $e){
+        } catch (QueryException $e) {
             return response("QueryException - Fixme.\n", 400);
         }
         return redirect()->back();
@@ -102,62 +102,70 @@ class VetController extends Controller
 
     //update method does not remove old image file from storage
     //fixme
-    public function update(Request $request){
+    public function update(Request $request)
+    {
         $current_user = auth()->user();
         date_default_timezone_set('Asia/Jerusalem');
         $vet_log = CatVetLog::find($request->to_logID);
 
-        if($vet_log == null)
+        if ($vet_log == null)
             return response()->json("Vet log not found");
 
-        if($request->to_date == null){
+        if ($request->to_date == null) {
             return response()->json("Visit date cannot be null");
-        }else{
+        } else {
             $vet_log->visit_date = $request->to_date;
         }
 
-        $vet_log->visit_date = $request->to_date;
         $vet_log->subject = $request->to_subject;
         $vet_log->description = $request->to_desc;
         $vet_log->clinic_name = $request->to_clinic;
         $vet_log->price = $request->to_price;
 
-//        $vet_log->prescription_picture = $request->prescription_picture;
-//        try{
-//            if(!empty($request->prescription_picture)){
-//                $vet_log->prescription_picture = str_replace(
-//                        ["@", "."],
-//                        "_",
-//                        $current_user->email."_".$vet_log->visit_date
-//                    ).".".$request->prescription_picture->getClientOriginalExtension();
-//            }
-//
-//            $vet_log->save();
-//            if (!empty($vet_log->prescription_picture)){
-//                Storage::disk("user_pictures")->putFileAs(
-//                    str_replace(["@", "."], "_", $current_user->email),
-//                    $request->prescription_picture,
-//                    $vet_log->prescription_picture
-//                );
-//            }
-//        }catch(QueryException $e){
-//            return response()->json("update failed");
-//        }
+        //
+        if ($request->to_pic != null) {
+            if ($vet_log->prescription_picture != null) {
+                $picture_path = str_replace(["@", "."], "_", $current_user->email);
+                Storage::disk("user_pictures")->delete($picture_path . "/" . $vet_log->prescription_picture);
+                $vet_log->prescription_picture = null;
+            }
+            try {
+                if (!empty($request->to_pic)) {
+                    $vet_log->prescription_picture = str_replace(
+                            ["@", "."],
+                            "_",
+                            $current_user->email . "_" . $vet_log->cat_name . "_" .$vet_log->visit_date
+                        ) . "." . $request->to_pic->getClientOriginalExtension();
+                }
+                $vet_log->save();
+                if (!empty($vet_log->prescription_picture)) {
+                    Storage::disk("user_pictures")->putFileAs(
+                        str_replace(["@", "."], "_", $current_user->email),
+                        $request->to_pic,
+                        $vet_log->prescription_picture
+                    );
+                }
+            } catch (QueryException $e) {
+                return response("QueryException - Fixme.\n", 400);
+            }
+        }
         $vet_log->update();
-//        return redirect()->json(["id" => $request->to_logID]);
         return redirect()->back();
 
     }
 
-    //delete method does not remove image file from storage
-    //fixme
-    public function delete(Request $request){
+    public function delete(Request $request)
+    {
+        $current_user = Auth::User();
         $cat_vet_log = CatVetLog::find($request->id);
-        if(empty($cat_vet_log)){
+        $picture_path = str_replace(["@", "."], "_", $current_user->email);
+        if (empty($cat_vet_log)) {
             return response()->json("failed");
-        }else{
+        } else {
+            if ($cat_vet_log->prescription_picture != null) {
+                Storage::disk("user_pictures")->delete($picture_path . "/" . $cat_vet_log->prescription_picture);
+            }
             $cat_vet_log->delete();
-            Storage::delete('$cat_vet_log->prescription_picture');
             return response()->json($request->id);
         }
     }
@@ -191,28 +199,5 @@ class VetController extends Controller
             $sum = $sum + $expenses[$i];
         }
         return $sum;
-    }
-    //    Natalie
-    public function updateLog(Request $request){
-        $currentUser = Auth::User();
-        $my_log = $currentUser->catVetLogs()->where('id', $request->id)->first();
-        if(empty($my_log))
-            return response("vetlog not found", 204);
-
-        $my_log->visit_date = $request->visit_date;
-        $my_log->subject = $request->subject;
-        $my_log->description = $request->description;
-        $my_log->clinic_name = $request->clinic_name;
-//        $my_log->prescription_image = $request->prescription_image;
-        $my_log->price = $request->price;
-
-        try{
-            $my_log->save();
-        }catch(QueryException $e){
-            return response("Update failed", 500);
-        }
-//        return response()->json(['visit_date' => $my_log->visit_date,'subject'=>$my_log->subject,'description' => $my_log->description,
-//        'clinic_name'=> $my_log->clinic_name, 'price'=> $my_log->price]);
-        return response()->json(['subject' => $my_log->subject]);
     }
 }
