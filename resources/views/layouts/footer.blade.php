9e59d7ec237aab5e57f7e96fdeeee17d414d9e19
SheyDenis <denis.sheyer@gmail.com>
2017-11-13 17:50:25 +0200

    Closes #93

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,3 @@
-<footer>
+<footer style="z-index: 100;">
     <p style="font-size: 8px">&copy 2015 Easy Admin Panel. All Rights Reserved | Design by <a href="https://w3layouts.com/" target="_blank">w3layouts.</a> - Stock images from <a href="http://www.iconka.com/" target="_blank">http://www.iconka.com</a></p>
 </footer>
